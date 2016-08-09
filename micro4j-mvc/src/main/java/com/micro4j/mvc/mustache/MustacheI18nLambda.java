b5c7d15a4f972bd586bddcca8c18ad290d4d3b5d
ui4j <admin@atolyemayis.com>
2016-08-09 15:10:18 +0300

    fixed NullPointerException

--- micro4j-mvc/src/main/java/com/micro4j/mvc/mustache/MustacheI18nLambda.java
+++ micro4j-mvc/src/main/java/com/micro4j/mvc/mustache/MustacheI18nLambda.java
@@ -22,7 +22,9 @@
  */
 package com.micro4j.mvc.mustache;
 
+import static com.micro4j.mvc.message.MvcMessages.getString;
 import static com.samskivert.mustache.Mustache.compiler;
+import static org.slf4j.LoggerFactory.getLogger;
 
 import java.io.IOException;
 import java.io.StringWriter;
@@ -30,6 +32,8 @@ import java.io.Writer;
 import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.concurrent.ConcurrentHashMap;
+
+import org.slf4j.Logger;
 
 import com.samskivert.mustache.Mustache.Compiler;
 import com.samskivert.mustache.Mustache.Lambda;
@@ -39,6 +43,8 @@ import com.samskivert.mustache.Template.Fragment;
 public class MustacheI18nLambda implements Lambda {
 
     private final Map<String, Template> templates = new ConcurrentHashMap<>();
+
+    private static final Logger LOG = getLogger(MustacheI18nLambda.class);
 
     public MustacheI18nLambda(ResourceBundle bundle) {
         Compiler compiler = compiler().escapeHTML(false);
@@ -53,6 +59,10 @@ public class MustacheI18nLambda implements Lambda {
     public void execute(Fragment frag, Writer out) throws IOException {
         String key = frag.execute().trim();
         Template template = templates.get(key);
+        if (template == null) {
+            LOG.error(getString("MustacheI18nLambda.key.not.found"), new Object[] { key });
+            return;
+        }
         Object parentContext = frag.context(1);
         StringWriter writer = new StringWriter();
         template.execute(frag.context(), parentContext, writer);
