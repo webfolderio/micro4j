91c0c0e6ad32ffdb7e3fd0ff9deda8c64a1e7b16
ui4j <admin@atolyemayis.com>
2016-11-09 15:00:57 +0300

    update the dependency versions & update the version to 1.4.0-SNAPSHOT

--- micro4j-samples/micro4j-mvc-sample/src/main/java/com/micro4j/sample/mvc/helloworld/HelloWorldController.java
+++ micro4j-samples/micro4j-mvc-sample/src/main/java/com/micro4j/sample/mvc/helloworld/HelloWorldController.java
@@ -22,16 +22,25 @@
  */
 package com.micro4j.sample.mvc.helloworld;
 
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
 
 import com.micro4j.mvc.View;
 
 @Path("/")
 public class HelloWorldController {
+
+    @GET
+    @Path("/")
+    public void inde() {
+        throw new WebApplicationException(Response.temporaryRedirect(URI.create("/hello")).build());
+    }
 
     @GET
     @Path("/hello")
