ea40e1688d5f3b6d004542779b36a6a253f67bce
ui4j <admin@atolyemayis.com>
2017-01-16 22:36:33 +0300

    fixed toCamelCase

--- micro4j-maven-plugin/src/main/java/com/micro4j/maven/plugin/InternalizationMojo.java
+++ micro4j-maven-plugin/src/main/java/com/micro4j/maven/plugin/InternalizationMojo.java
@@ -1,15 +1,29 @@
 package com.micro4j.maven.plugin;
 
+import static java.lang.Character.isUpperCase;
+import static com.squareup.javapoet.JavaFile.builder;
+import static com.squareup.javapoet.MethodSpec.constructorBuilder;
+import static com.squareup.javapoet.TypeSpec.anonymousClassBuilder;
+import static com.squareup.javapoet.TypeSpec.enumBuilder;
 import static java.io.File.separator;
 import static java.io.File.separatorChar;
+import static java.lang.Character.isLetterOrDigit;
+import static java.lang.Character.toLowerCase;
+import static java.lang.Character.toUpperCase;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static java.nio.file.Files.isDirectory;
 import static java.nio.file.Files.readAllBytes;
+import static javax.lang.model.element.Modifier.FINAL;
+import static javax.lang.model.element.Modifier.PRIVATE;
+import static javax.lang.model.element.Modifier.PUBLIC;
 import static org.apache.maven.plugins.annotations.LifecyclePhase.PROCESS_RESOURCES;
 
 import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.maven.model.Resource;
 import org.apache.maven.plugin.AbstractMojo;
@@ -21,6 +35,9 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.codehaus.plexus.util.Scanner;
 import org.sonatype.plexus.build.incremental.BuildContext;
+
+import com.squareup.javapoet.TypeSpec;
+import com.squareup.javapoet.TypeSpec.Builder;
 
 @Mojo(name = "i18n", defaultPhase = PROCESS_RESOURCES, threadSafe = true, requiresOnline = false, requiresReports = false)
 public class InternalizationMojo extends AbstractMojo {
@@ -36,6 +53,8 @@ public class InternalizationMojo extends AbstractMojo {
 
     @Parameter(defaultValue = "**/*.properties")
     private String[] i18nIncludes = new String[] { "**/*.properties" };
+
+    private static final String EQUAL = "=";
 
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
@@ -71,10 +90,9 @@ public class InternalizationMojo extends AbstractMojo {
             className = includedFile.substring(start + 1, includedFile.length());
             int end = className.indexOf('.');
             if (start > 0) {
-                className = EnumGenerator.toCamelCase(className.substring(0, end));
+                className = toCamelCase(className.substring(0, end));
             }
-            EnumGenerator generator = new EnumGenerator(packageName, className, enumField);
-            String source = generator.generate(content);
+            String source = generate(content, packageName, className, enumField);
             if (source == null || source.trim().isEmpty()) {
                 continue;
             }
@@ -104,5 +122,72 @@ public class InternalizationMojo extends AbstractMojo {
                 throw new MojoExecutionException(e.getMessage(), e);
             }
         }
+    }
+
+    protected static String toCamelCase(final String name) {
+        final StringBuilder builder = new StringBuilder();
+        boolean capitalizeNextChar = false;
+        boolean first = true;
+        for (int i = 0; i < name.length(); i++) {
+            final char c = name.charAt(i);
+            if (!isLetterOrDigit(c)) {
+                if (!first) {
+                    capitalizeNextChar = true;
+                }
+            } else {
+                if (capitalizeNextChar || first || isUpperCase(c)) {
+                    builder.append(toUpperCase(c));
+                } else {
+                    builder.append(toLowerCase(c));
+                }
+                capitalizeNextChar = false;
+                first = false;
+            }
+        }
+        return builder.toString();
+    }
+
+    protected String generate(String content,
+                                    String packageName,
+                                    String className,
+                                    String fieldName) throws MojoExecutionException {
+        List<String> keys = new ArrayList<>();
+        try (java.util.Scanner scanner = new java.util.Scanner(content)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                int start = line.indexOf(EQUAL);
+                if (start <= 0) {
+                    continue;
+                }
+                String key = line.substring(0, start);
+                keys.add(key);
+            }
+        }
+        Builder builder = enumBuilder(className)
+                                .addField(String.class, fieldName, FINAL, PUBLIC)
+                                .addMethod(
+                                            constructorBuilder()
+                                                .addModifiers(PRIVATE)
+                                                .addParameter(String.class, fieldName)
+                                                .addStatement("this.$N = $N", fieldName, fieldName)
+                                            .build())
+                                .addModifiers(PUBLIC);
+
+        for (String key : keys) {
+            builder.addEnumConstant(
+                            toCamelCase(key),
+                                anonymousClassBuilder("$S", key.trim())
+                            .build());
+        }
+        TypeSpec spec = builder.build();
+        StringWriter writer = new StringWriter();
+        try {
+            builder(packageName, spec)
+                    .build()
+                    .writeTo(writer);
+        } catch (IOException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+        return writer.toString();
     }
 }
