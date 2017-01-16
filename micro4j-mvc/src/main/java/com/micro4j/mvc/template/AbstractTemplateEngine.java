f0275b43ffbd64e7242cd66acb30abb2f48b90bb
ui4j <admin@atolyemayis.com>
2017-01-16 21:38:06 +0300

    update the copyright date

--- micro4j-mvc/src/main/java/com/micro4j/mvc/template/AbstractTemplateEngine.java
+++ micro4j-mvc/src/main/java/com/micro4j/mvc/template/AbstractTemplateEngine.java
@@ -1,6 +1,6 @@
 /* MIT License
  * 
- * Copyright (c) 2016 http://micro4j.com
+ * Copyright (c) 2016 - 2017 http://micro4j.com
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
